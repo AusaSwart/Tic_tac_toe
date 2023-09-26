02767d94bcb3cfea8d58345455ad6dc18a6bb9dc
AusaSwart <october.monroe@mail.ru>
2023-09-26 18:08:00 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        // intro
+        System.out.println("  Welcome to the game Tic-tac-toe");
+
+
+
+        
+    }
+}
