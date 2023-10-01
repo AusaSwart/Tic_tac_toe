6ca1c1ee4fdfc5fbf28875affb8243737639aed9
AusaSwart <october.monroe@mail.ru>
2023-10-01 17:42:23 +0300

    work, but incorrect

--- src/TicTac.java
+++ src/TicTac.java
@@ -1,15 +1,18 @@
 import java.util.Random;
 import java.util.Scanner;
 
+@SuppressWarnings("BooleanMethodIsAlwaysInverted")
 public class TicTac {
 
-    final char Xchar = 'X';
-    final char Ochar = 'O';
+    // symbolic constants
+    final char XChar = 'X';
+    final char OChar = 'O';
     final char Empty = '_';
-    char board[][];
+    char[][] board;
     Scanner scanner;
     Random random;
 
+    // entrance in program
     public static void main(String[] args) {
         // start game
         new TicTac().game();
@@ -21,45 +24,122 @@ public class TicTac {
         scanner = new Scanner(System.in);
         board = new char[3][3];
     }
+
+    // game logic
     void game() {
-        // game logic
+        // init
+        initBoard();
+        // infinity loop
+        while (true) {
+            // human turn
+            playerOne();
+            // check
+            if (check(XChar)) {
+                System.out.println(" Player One Win");
+                break;
+            }
+            if (boardFull()) {
+                System.out.println(" Draw");
+                break;
+            }
+            // machine turn
+            playerTwo();
+            drawBoard();
+            // check
+            if (check(OChar)) {
+                System.out.println(" Player Two Win");
+                break;
+            }
+            if (boardFull()) {
+                System.out.println(" Draw");
+                break;
+            }
+        }
+        System.out.println(" Game over");
+        drawBoard();
+    }
 
+    // player one move
+    private void playerOne() {
+        int x, y;
+        do {
+            System.out.println(" Enter x - row & y - cow, coordinates ( 1, 2, 3 ): ");
+            x = scanner.nextInt() - 1;
+            y = scanner.nextInt() - 1;
+        } while (!isValidCell(x, y));
+        board[x][y] = XChar;
+    }
 
+    // check coordinates of cell, if they are valid
+    private boolean isValidCell(int x, int y) {
+        if (( x < 0 || y < 0 || x >= 3 || y >= 3 ) && board[x][y] != Empty) {
+            System.out.println(" Incorrect coordinates, try again");
+            return false;
+        }
+        return true;
+    }
+
+    // player two move
+    private void playerTwo() {
+        int x, y;
+        do {
+            x = random.nextInt(3);
+            y = random.nextInt(3);
+        } while (!isValidCell(x, y));
+        board[x][y] = OChar;
+    }
+
+    // check if board full
+    private boolean boardFull() {
+        for (int i = 0; i < 3; i++){
+            for (int j = 0; j < 3; j++){
+                if (board[i][j] == Empty){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    // win check
+    private boolean check(char w) {
+        if (board[1][1] == w &&
+                ((board[0][0] == w && board[2][2] == w) ||
+                 (board[2][0] == w && board[0][2] == w))){
+            return true;
+        }
+        for (int i = 0; i < 3; i++ ){
+            if ( (board[0][i] == w && board[1][i] == w && board[2][i] == w) ||
+                 (board[i][0] == w && board[i][1] == w && board[i][2] == w) ) {
+                return true;
+            }
+        }
+        return false;
 
     }
 
+    // board initialization
     void initBoard() {
-        for (){
-            for (){
-
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = Empty;
             }
         }
     }
 
+    // draw a board
     void drawBoard() {
-        for (){
-            for (){
-
+        for (int i = 0; i < 3; i++) {
+            System.out.print(" ");
+            for (int j = 0; j < 3; j++) {
+                System.out.print(board[i][j]);
+                if( j < 2) System.out.print(" | ");
             }
+            if (i < 2 ) System.out.println("\n____________");
         }
+        System.out.println("\n\n");
     }
-
-
-
 }
 
 
-
-
-
-
-
-
-//       create board  (step1) - done
-//       here we can see our board (step2) - done
-// tracking correct symbols and players turn (step3)
-// asking players their step before they are become valid (step4)
-// set symbol on place of board (step5)
-// win check (step6)
-// check game on tie (step7)
-//       loop to keep going (step8) - done
+// optimization: add human player
